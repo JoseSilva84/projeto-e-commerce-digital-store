19835cc50893eb07b3979993c10bb8b1b1687dfd
José Evangelista da Silva Filho <juniornyanata@gmail.com>
2025-06-09 16:42:18 -0300

    mudanca8

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import "primeicons/primeicons.css";
 import './App.css'
 import Header from './components/Header';
 import Footer from "./components/Footer";
-import Section from "./components/Section";
 import Carrosel from "./components/Carrosel";
 import Colecoes from "./components/colecoes";
 import Camiseta from "./components/camiseta";
@@ -17,7 +16,6 @@ function App() {
   return (
     <>
       <Header />
-      {/* <Section /> */}
       <Carrosel />
       <Colecoes />
       <Camiseta />
