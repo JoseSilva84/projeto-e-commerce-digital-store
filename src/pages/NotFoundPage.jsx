bab4856b0bf80299f2446cbfa1e22fdf24f75cb7
José Evangelista da Silva Filho <juniornyanata@gmail.com>
2025-06-26 23:22:55 -0300

    mudanca30

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,10 @@
 import Layout from "../pages/Layout";
 import 'primeicons/primeicons.css';
+import { Button } from 'primereact/button';
+import { useNavigate } from "react-router-dom";
 
 const NotFoundPage = () => {
+    const navigate = useNavigate();
     return (
         <div>
             <Layout>
@@ -9,6 +12,9 @@ const NotFoundPage = () => {
                     <i className="pi pi-exclamation-triangle" style={{ fontSize: '5rem', color: 'pink', marginBottom: '12px' }}></i>
                     <h1 className="text-pink-600 text-4xl font-bold mb-4">404</h1>
                     <p className="text-pink-600 text-lg mb-4">Página não encontrada</p>
+                    <Button className="!bg-pink-600 hover:!bg-pink-700 !text-white !px-8 !py-2 rounded-lg !border-0 !shadow-none" onClick={() => navigate(-1)}>
+                        Voltar
+                    </Button>
                 </div>
             </Layout>
         </div>
